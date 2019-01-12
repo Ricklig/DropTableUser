b6e5eef6e71ebd04ed66f92cca3fe467f30bbc8a
chumjo <cynthia.cherrier@hotmail.com>
2019-01-12 00:43:19 -0500

    Added script for pause menu.

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public bool isPaused;
+    public GameObject PauseMenuUI;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused == true)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+        else if (Time.timeScale == 1f)
+            Resume();
+    }
+
+    public void Resume()
+    {
+        isPaused = false;
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    private void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        isPaused = true;
+        Time.timeScale = 0f;
+    }
+
+    public void Options()
+    {
+        SceneManager.LoadScene("Menu", LoadSceneMode.Single);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
