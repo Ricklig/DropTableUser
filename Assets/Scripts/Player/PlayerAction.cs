d3978fb0b6918dd499acb53eac7bfc61f14aeea4
Ricardo Liganor <ricardo.liganor@hotmail.com>
2019-01-12 15:34:41 -0500

    fixed sphere cast

--- Assets/Scripts/Player/PlayerAction.cs
+++ Assets/Scripts/Player/PlayerAction.cs
@@ -40,10 +40,10 @@ public class PlayerAction : MonoBehaviour {
     private void Action()
     {
         Vector3 startPos = transform.position; // umm, start position !
-        Vector3 targetPos = transform.position + transform.forward; // variable for calculated end position
+        Vector3 targetPos =  transform.forward; // variable for calculated end position
 
         RaycastHit tg;
-        if(Physics.SphereCast(startPos, 5.5f, targetPos, out tg, 10f))
+        if(Physics.SphereCast(startPos, .5f, targetPos, out tg, 1.5f))
         {
             Debug.Log(tg.collider);
             if (tg.collider.gameObject.tag.Equals("Art"))
@@ -52,39 +52,6 @@ public class PlayerAction : MonoBehaviour {
                 tg.collider.gameObject.tag = "Stolen";
             }
         }
-        Debug.DrawLine(startPos, targetPos, Color.green, 10f);
-
-
-
-        /*
-         float angle = Vector2.Angle(startPos, targetPos) + 30;
-
-         int startAngle = (int)(-angle * 0.5); // half the angle to the Left of the forward
-         int finishAngle = (int)(angle * 0.5); // half the angle to the Right of the forward
-
-         // the gap between each ray (increment)
-         int inc = (int)(30 / 5);
-
-
-         // step through and find each target point
-         for (int i = startAngle; i < finishAngle; i += inc) // Angle from forward
-         {
-             targetPos = (Quaternion.Euler(0, i, 0) * transform.forward).normalized * 1.5f + transform.position;
-
-             RaycastHit hit;
-             if(Physics.Linecast(startPos, targetPos, out hit))
-                 {
-                     if(hit.collider.gameObject.tag.Equals("Art"))
-                     {
-                          gm.addValue(hit.collider.GetComponent<Value>().getValue());
-                          hit.collider.gameObject.tag = "Stolen";
-                      }
-                 }
-
-
-
-             // to show ray just for testing
-             Debug.DrawLine(startPos, targetPos, Color.green, 5.5f);
-         }*/
+        
     }
 }
