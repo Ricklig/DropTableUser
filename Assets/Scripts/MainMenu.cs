2a1baf76d87cf6c7f393b4bc255d036460eb3917
Vincent Fortin <laurent.caron.2@ens.etsmtl.ca>
2019-01-11 22:23:06 -0500

    Created script for main menu.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+    
+    int indexResolution;
+    int screenWidth, screenHeight;
+    int btnWidth, btnHeight;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
