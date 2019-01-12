3d0a3e5be40ec3a7d9677880cfe41b1f87716737
Ricardo Liganor <ricardo.liganor@hotmail.com>
2019-01-12 03:05:49 -0500

    Player work

---
+++ Assets/Scripts/PlayerAction.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAction : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Interact"))
+        {
+            Action();
+        }
+    }
+
+    private void Action()
+    {
+        Vector3 startPos = transform.position; // umm, start position !
+        Vector3 targetPos = transform.position + transform.forward; // variable for calculated end position
+
+        float angle = Vector2.Angle(startPos, targetPos) + 30;
+
+        int startAngle = (int)(-angle * 0.5); // half the angle to the Left of the forward
+        int finishAngle = (int)(angle * 0.5); // half the angle to the Right of the forward
+
+        // the gap between each ray (increment)
+        int inc = (int)(30 / 5);
+
+
+        // step through and find each target point
+        for (int i = startAngle; i < finishAngle; i += inc) // Angle from forward
+        {
+            targetPos = (Quaternion.Euler(0, i, 0) * transform.forward).normalized * 1.5f + transform.position;
+
+            RaycastHit hit;
+            if(Physics.Linecast(startPos, targetPos, out hit))
+                {
+                    if(hit.collider.gameObject.tag.Equals("Art"))
+                    {
+                        Debug.Log("hit");
+                    hit.collider.gameObject.tag = "Stolen";
+                     }
+                }
+            
+
+
+            // to show ray just for testing
+            Debug.DrawLine(startPos, targetPos, Color.green, 5.5f);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+    //for internal referencing
+    private Rigidbody playerRB;
+    private bool sprint;
+
+    // Use this for initialization
+    void Start () {
+        //Get rigidbody
+        playerRB = GetComponent<Rigidbody>();
+        sprint = false;
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Move();
+
+        if (Input.GetButtonDown("Sprint"))
+        {
+            sprint = true;
+        }
+        if (Input.GetButtonUp("Sprint"))
+        {
+            sprint = false;
+        }
+	}
+
+    //move script
+    private void Move()
+    {
+        float moveHorizontal = 0;
+        float moveVertical = 0;
+        if (sprint)
+        {
+            moveHorizontal = Input.GetAxis("Horizontal") * Time.deltaTime * 5.0f;
+            moveVertical = Input.GetAxis("Vertical") * Time.deltaTime * 5.0f;
+        }
+        else
+        {
+            moveHorizontal = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f;
+            moveVertical = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+        }
+        
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f , moveVertical);
+        playerRB.transform.Translate(movement);
+
+        //Get the Screen positions of the object
+        Vector2 positionOnScreen = Camera.main.WorldToViewportPoint(transform.position);
+
+        //Get the Screen position of the mouse
+        Vector2 mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
+
+        //Get the angle between the points
+        float angle = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen);
+
+        //Ta Daaa
+        playerRB.transform.rotation = Quaternion.Euler(new Vector3(0f, angle ,0f ));
+    }
+
+    float AngleBetweenTwoPoints(Vector3 a, Vector3 b)
+    {
+        return Mathf.Atan2(a.y - b.y, a.x - b.x) * -Mathf.Rad2Deg;
+    }
+
+    public bool GetSprint()
+    {
+        return sprint;
+    }
+}
