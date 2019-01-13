11398bcf50013cfecc7ae0c513b8185ca328cbda
Vincent Fortin <vincent-2.fortin@polymtl.ca>
2019-01-13 00:40:39 -0500

    More stuff.

---
+++ Assets/Scripts/Menus/RotateItem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateItem : MonoBehaviour {
+
+    int rotateRate = 20;
+
+	// Use this for initialization
+	void Start () {
+        rotateRate = Random.Range(15, 40);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(0, Time.deltaTime * rotateRate, 0);
+	}
+}
