0816fd9e5fd3146f8882cf3bdd48a31328f25a9d
Lumel <claire.quesnel@mail.mcgill.ca>
2019-01-13 16:12:46 -0500

    node seethrough

--- Assets/Scripts/AI/AlertManager.cs
+++ Assets/Scripts/AI/AlertManager.cs
@@ -4,9 +4,9 @@ using System.Collections.Generic;
 using UnityEngine;
 public enum AlertStatus { level1 = 1, level2 = 2, level3 = 3, RedAlert = 4 }
 public class AlertManager{
-    public int AlertLevel2Ceil = 1;
-    public int AlertLevel3Ceil = 2;
-    public int RedAlertCeil = 3;
+    public int AlertLevel2Ceil = 3;
+    public int AlertLevel3Ceil = 5;
+    public int RedAlertCeil = 8;
     public AlertStatus currentAlertStatus = AlertStatus.level1;
     public delegate void AlertStatusChanged(AlertStatus alertStatus);
     public event AlertStatusChanged alertStatusChanged;
